185ddfc29d22c480b12a176fc0d99b728091198c
Ivan <ivandasilvamartin1@gmail.com>
2023-10-02 19:34:13 +0100

    hacer partial del formulario de creacion de links

--- resources/views/community/index.blade.php
+++ resources/views/community/index.blade.php
@@ -18,38 +18,7 @@
             </div>
             {{-- Right colum to show the form to upload a link --}}
             <div class="col-md-4">
-                <div class="card">
-                    <div class="card-header">
-                        <h3>Contribute a link</h3>
-                    </div>
-                    <div class="card-body">
-                        <form method="POST" action="/community">
-                            @csrf
-                            <div class="form-group">
-                                <label for="title">Title:</label>
-                                <input type="text" class="form-control" id="title" name="title"
-                                    placeholder="What is the title of your article?">
-                                @error('title')
-                                    <div class="alert alert-danger">{{ 'Titulo Incorrect' }}</div>
-                                @enderror
-                            </div>
-
-                            <div class="form-group">
-                                <label for="link">Link:</label>
-                                <input type="text" class="form-control" id="link" name="link"
-                                    placeholder="What is the URL?">
-                                @error('link')
-                                    <div class="alert alert-danger">{{ "link incorrecto" }}</div>
-                                @enderror
-                            </div>
-
-                            <div class="form-group pt-3">
-                                <button class="btn btn-primary">Contribute!</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-
+                @include('add-link')
             </div>
         </div>
         {{ $links->links() }}
